f691b2373100a9282dd2602825d7d22180b90ee2
Shu Chuan Yao <32171168+raymond800108@users.noreply.github.com>
2020-05-26 17:52:19 +0800

    Create OLED_Display.ino

---
+++ OLED_Display.ino
@@ -0,0 +1,156 @@
+
+
+#include <Servo.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
+//These values are in the datasheet
+#define RT0 100000   // Ω
+#define B 3950      // K
+//--------------------------------------
+
+#define VCC 5    //Supply voltage
+#define R 100000  //R=100KΩ
+
+//Variables
+float RT, VR, ln, TX, T0, VRT;//These values are in the datasheet
+Servo myservo;
+int sensorValue;
+
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000
+};
+
+void setup() {
+  myservo.attach(8);
+  myservo.write(0);
+
+
+  Serial.begin(9600);
+  T0 = 25 + 273.15;                 //Temperature T0 from datasheet, conversion from Celsius to kelvin
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+  // display.display() is NOT necessary after every single drawing command,
+  // unless that's what you want...rather, you can batch up a bunch of
+  // drawing operations and then update the screen all at once by calling
+  // display.display(). These examples demonstrate both approaches...
+
+}
+
+void loop() {
+
+  testdrawstyles();    // Draw 'stylized' characters
+  termistorSensing();
+
+  if (TX > 140.0) {
+    myservo.write(0);
+  }
+  else if (TX > 100.0) {
+    myservo.write(70);
+  }
+  else if (TX < 100.0) {
+    myservo.write(0);
+
+  }
+
+
+
+
+}
+
+
+void termistorSensing(void) {
+  VRT = analogRead(A0);              //Acquisition analog value of VRT
+  VRT = (5.00 / 1023.00) * VRT;      //Conversion to voltage
+  VR = VCC - VRT;
+  RT = VRT / (VR / R);               //Resistance of RT
+
+  ln = log(RT / RT0);
+  TX = (1 / ((ln / B) + (1 / T0))); //Temperature from thermistor
+
+  TX = TX - 273.15;                 //Conversion to Celsius
+
+  Serial.print("Temperature:");
+  Serial.print("\t");
+  Serial.print(TX);
+  Serial.print("C\t\t");
+  Serial.print(TX + 273.15);        //Conversion to Kelvin
+  Serial.print("K\t\t");
+  Serial.print((TX * 1.8) + 32);    //Conversion to Fahrenheit
+  Serial.println("F");
+  delay(500);
+
+}
+
+void testdrawstyles(void) {
+
+
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0, 0);            // Start at top-left corner
+  display.println(F("Heated-Bed Degree"));
+
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE); // Draw 'inverse' text
+  display.println(TX);
+
+
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE); // Draw 'inverse' text
+  display.println(F("Pressure:"));
+  display.print(sensorValue);
+
+
+  display.display();
+  delay(1000);
+
+
+}
